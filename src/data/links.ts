e9593ca639a2c7052853dd9cf43e4877f884476d
Ricky Tang <tangricky001@gmail.com>
2025-08-06 21:07:24 -0400

    fix: addressed the links and enhanced the styles in the footer

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,7 @@ export const externalLinks = {
   github: "https://github.com/rickytang666",
   linkedin: "https://www.linkedin.com/in/ricky-tang-dev/",
   websiteRepo: "https://github.com/rickytang666/rickytang-website",
-  email: "tangricky001@gmail.com",
+  email: "mailto:tangricky001@gmail.com",
   devpost: "https://devpost.com/tangricky001",
   leetcode: "https://leetcode.com/u/tangricky001/",
 };
